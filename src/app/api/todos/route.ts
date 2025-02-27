3f69477c6a353af2b6719e24f2e9a30b959bdc0d
Luciano Figueroa <lumusitech@gmail.com>
2025-02-27 01:44:25 -0300

    feat: update get todos endpoint with pagination

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -2,16 +2,22 @@ import prisma from '@/lib/prisma'
 import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
-  const query = new URL(request.url).searchParams
+  const { searchParams } = new URL(request.url)
 
-  const take = query.get('take') ?? '10'
-  const offset = query.get('offset') ?? '0'
+  const take = Number(searchParams.get('take') ?? '10')
+  const skip = Number(searchParams.get('skip') ?? '0')
 
-  console.log({ take, offset })
+  if (isNaN(take)) {
+    return NextResponse.json({ message: 'take must be a number', status: 400 })
+  }
+
+  if (isNaN(skip)) {
+    return NextResponse.json({ message: 'skip must be a number', status: 400 })
+  }
 
   const todos = await prisma.todo.findMany({
-    take: parseInt(take),
-    skip: parseInt(offset),
+    take,
+    skip,
   })
 
   return NextResponse.json({ todos })
