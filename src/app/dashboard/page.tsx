ffaeb5bbcd3ef6c5072c1c977d6c866e3fed4994
Luciano Figueroa <lumusitech@gmail.com>
2025-02-27 14:23:57 -0300

    feat: add base design for dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function DashboardPage() {
+  return (
+    <div>
+      <h1 className='text-5xl'>Dashboard</h1>
+    </div>
+  )
+}
---
+++ src/app/dashboard/rest-todos/page.tsx
@@ -0,0 +1,7 @@
+export default function RestTodosPage() {
+  return (
+    <div>
+      <h1 className='text-5xl'>Rest Todos page</h1>
+    </div>
+  )
+}
