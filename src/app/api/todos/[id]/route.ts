a636fa4a5bb6dc017d6978490e887845c0909ba7
Luciano Figueroa <lumusitech@gmail.com>
2025-02-27 03:09:51 -0300

    feat: add update todo endpoint with yup validations

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,20 +1,51 @@
 import prisma from '@/lib/prisma'
+import { Todo } from '@prisma/client'
 import { NextResponse } from 'next/server'
+import { boolean, object, string } from 'yup'
 
 interface Segments {
   params: {
     id: string
   }
 }
+const getTodo = async (id: string): Promise<Todo | null> => {
+  return await prisma.todo.findUnique({ where: { id } })
+}
 
-export async function GET(request: Request, { params }: Segments) {
-  const { id } = params
-
-  const todo = await prisma.todo.findUnique({ where: { id } })
+export async function GET(request: Request, segments: Segments) {
+  const params = await segments.params
+  const todo = await getTodo(params.id)
 
   if (!todo) {
-    return NextResponse.json({ message: `Todo with id ${id} is not exists`, status: 404 })
+    return NextResponse.json({ message: `Todo with id ${params.id} is not exists`, status: 404 })
   }
 
   return NextResponse.json({ todo })
+}
+
+const putSchema = object({
+  description: string().optional(),
+  completed: boolean().optional().default(false),
+})
+
+export async function PUT(request: Request, segments: Segments) {
+  const params = await segments.params
+  const todo = await getTodo(params.id)
+
+  if (!todo) {
+    return NextResponse.json({ message: `Todo with id ${params.id} is not exists`, status: 404 })
+  }
+
+  try {
+    const { description, completed } = await putSchema.validate(await request.json())
+
+    const updatedTodo = await prisma.todo.update({
+      where: { id: params.id },
+      data: { description, completed },
+    })
+
+    return NextResponse.json(updatedTodo)
+  } catch (error) {
+    return NextResponse.json({ error, status: 400 })
+  }
 }
