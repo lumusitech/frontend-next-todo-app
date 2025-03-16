0b280571fafffa5452506afeb296c07d4e14cb4e
Luciano Figueroa <lumusitech@gmail.com>
2025-03-16 14:43:34 -0300

    feat: add basic info of logged user

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,31 @@
+import { WidgetItem } from '@/components'
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
+import { authOptions } from '../api/auth/[...nextauth]/route'
+
 export const metadata = {
   title: 'Dashboard Page',
   description: 'Dashboard Page',
 }
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const session = await getServerSession(authOptions)
+
+  if (!session) {
+    redirect('/api/auth/signin')
+  }
+
   return (
     <>
-      <div className='grid gap-6 md:grid-cols-2 lg:grid-cols-3'>
+      <div className='grid gap-6 grid-cols-1 sm:grid-cols-2'>
         {/* TODO: Posibly replace with widgetsGrid component */}
-        {/* <WidgetItem /> */}
+        <WidgetItem title='User Connected - Server Side'>
+          <div className='flex flex-col'>
+            <span>{session.user?.name}</span>
+            <span>{session.user?.image}</span>
+            <span>{session.user?.email}</span>
+          </div>
+        </WidgetItem>
       </div>
     </>
   )
