a3d9b018fa6189239e380f44f25bef2a3b27e7e9
Luciano Figueroa <lumusitech@gmail.com>
2025-03-15 21:28:23 -0300

    feat: add interactivity to cart button on top menu

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,4 +1,5 @@
 import { cookies } from 'next/headers'
+import Link from 'next/link'
 import { CiChat1, CiMenuBurger, CiSearch, CiShoppingBasket } from 'react-icons/ci'
 
 interface Cart {
@@ -47,12 +48,15 @@ export const TopMenu = async () => {
             <button className='flex items-center justify-center w-10 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200'>
               <CiChat1 size={25} />
             </button>
-            <button className='flex items-center justify-center h-10 p-2 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200'>
+            <Link
+              href={'/dashboard/cart'}
+              className='flex items-center justify-center h-10 p-2 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200'
+            >
               {totalItems > 0 && (
                 <span className='text-sm mr-2 font-bold text-blue-800'>{totalItems}</span>
               )}
               <CiShoppingBasket size={25} />
-            </button>
+            </Link>
           </div>
         </div>
       </div>
--- src/shopping-cart/index.ts
+++ src/shopping-cart/index.ts
@@ -1,1 +1,2 @@
 export * from './actions/actions'
+export * from './components/itemCard'
