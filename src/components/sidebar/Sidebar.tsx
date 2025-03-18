e5f3baeb791da67f8d8a47976a4f6d8fef614ee3
Luciano Figueroa <lumusitech@gmail.com>
2025-03-17 22:15:18 -0300

    feat: add functionality to bottom button in sidebar for loggin logout

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -3,7 +3,6 @@ import { getServerSession } from 'next-auth'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import { CiLogout } from 'react-icons/ci'
 import {
   IoAtCircleOutline,
   IoBasketOutline,
@@ -12,6 +11,7 @@ import {
   IoListOutline,
   IoPersonOutline,
 } from 'react-icons/io5'
+import { LogoutButton } from './LogoutButton'
 import { SidebarItem } from './SidebarItem'
 
 interface Item {
@@ -97,10 +97,7 @@ export const Sidebar = async () => {
       </div>
 
       <div className='px-6 -mx-6 pt-4 flex justify-between items-center border-t'>
-        <button className='px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group'>
-          <CiLogout />
-          <span className='group-hover:text-gray-700'>Logout</span>
-        </button>
+        <LogoutButton />
       </div>
     </aside>
   )
