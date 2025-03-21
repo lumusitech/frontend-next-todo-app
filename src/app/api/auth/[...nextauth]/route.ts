579aef3fbe2e8ec66de30c458789d9c9d51be794
Luciano Figueroa <lumusitech@gmail.com>
2025-03-21 19:57:41 -0300

    feat: add functionality to save user data from providers google, github

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,11 @@
+import prisma from '@/lib/prisma'
+import { PrismaAdapter } from '@auth/prisma-adapter'
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
 
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
