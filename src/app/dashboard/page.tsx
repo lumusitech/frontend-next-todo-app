292262670257760e86cbd84a0b15567029c8a839
Luciano Figueroa <lumusitech@gmail.com>
2025-03-27 22:00:25 -0300

    feat: change dashboard styles to get a bigger widget for dev

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -17,13 +17,15 @@ export default async function DashboardPage() {
 
   return (
     <>
-      <div className='grid gap-6 grid-cols-1 sm:grid-cols-2'>
+      <div className='grid gap-6 grid-cols-1'>
         {/* TODO: Posibly replace with widgetsGrid component */}
         <WidgetItem title='User Connected - Server Side'>
           <div className='flex flex-col'>
             <span>{session.user?.name}</span>
             <span>{session.user?.image}</span>
             <span>{session.user?.email}</span>
+
+            <pre>{JSON.stringify(session, null, 2)}</pre>
           </div>
         </WidgetItem>
       </div>
